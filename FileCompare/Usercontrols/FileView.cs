20d16a745a7a4197849e5c9b936795acfb9f458e
PaxSentry <paxsentry@gmail.com>
2017-04-09 16:30:09 +0100

    Logger added

--- FileCompare/Usercontrols/FileView.cs
+++ FileCompare/Usercontrols/FileView.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using NLog;
+using System;
+using System.IO;
 using System.Windows.Forms;
 
 namespace FileCompare
 {
     public partial class UCFileView : UserControl
     {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
         public UCFileView()
         {
             InitializeComponent();
@@ -13,13 +17,32 @@ namespace FileCompare
         private void toolStripButtonOpenFile_Click(object sender, EventArgs e)
         {
             var dialog = new OpenFileDialog();
-            dialog.Filter = "Rich Text files|*.rtf";
+            dialog.Filter = "Rich Text files|*.rtf|Simple Text files|*.txt|XML files|*.xml";
             var result = dialog.ShowDialog();
-
-            if (result == DialogResult.OK)
+            try
             {
-                this.richTBFileView.LoadFile(dialog.FileName, RichTextBoxStreamType.RichText);
-                this.Text = dialog.FileName;
+                if (result == DialogResult.OK)
+                {
+                    if (Path.GetExtension(dialog.FileName) == "*.rtf")
+                    {
+                        this.richTBFileView.LoadFile(dialog.FileName, RichTextBoxStreamType.RichText);
+                        this.Text = dialog.FileName;
+                    }
+                    if (Path.GetExtension(dialog.FileName) == "*.txt")
+                    {
+                        this.richTBFileView.LoadFile(dialog.FileName, RichTextBoxStreamType.PlainText);
+                        this.Text = dialog.FileName;
+                    }
+                    if (Path.GetExtension(dialog.FileName) == "*.xml")
+                    {
+                        // TODO add xml loading and parsing functions
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Exception during loading file. {ex.Message}");
+                throw;
             }
         }
     }
