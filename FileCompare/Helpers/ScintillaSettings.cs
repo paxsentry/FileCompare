9105abd43c3f3bbfd24e8519be5ae011f08b6378
PaxSentry <zoltan.stomfai@gmail.com>
2017-04-25 19:16:38 +0100

    Refactor finished, clean, working solution.

---
+++ FileCompare/Helpers/ScintillaSettings.cs
@@ -0,0 +1,114 @@
+﻿using System.Drawing;
+using ScintillaNET;
+
+namespace FileCompare.Helpers
+{
+    public static class ScintillaSettings
+    {
+        private static Scintilla _area;
+        private static int _maxLineNumberCharLength;
+
+        public static void SetupScintilla(Scintilla area)
+        {
+            _area = area;
+            SetupBasics();
+            SetupSelectionBackColor();
+            SetupLineNumbering();
+            SetupXmlSyntaxColoring();
+        }
+
+        public static void SetupXML(Scintilla area)
+        {
+            _area = area;
+            SetupXmlSyntaxColoring();
+            SetupLineNumbering();
+        }
+
+        private static void SetupBasics()
+        {
+            _area.Dock = System.Windows.Forms.DockStyle.Fill;
+        }
+
+        private static void SetupSelectionBackColor()
+        {
+            _area.SetSelectionBackColor(true, IntToColor(0x114D9C));
+        }
+
+        private static void SetupLineNumbering()
+        {
+            var maxLineNumberCharLength = _area.Lines.Count.ToString().Length;
+            if (maxLineNumberCharLength == _maxLineNumberCharLength)
+                return;
+
+            // Calculate the width required to display the last line number and include some padding for good measure.
+            const int padding = 1;
+            _area.Margins[0].Width = _area.TextWidth(Style.LineNumber, new string('9', maxLineNumberCharLength + 1)) + padding;
+            _maxLineNumberCharLength = maxLineNumberCharLength;
+        }
+
+        private static void SetupXmlSyntaxColoring()
+        {
+            // Reset the styles
+            _area.StyleResetDefault();
+            _area.Styles[Style.Default].Font = "Consolas";
+            _area.Styles[Style.Default].Size = 10;
+            _area.Styles[Style.Default].BackColor = Color.DarkGray;
+            _area.Styles[Style.Default].ForeColor = Color.DarkBlue;
+            _area.StyleClearAll();
+
+            // Set the XML Lexer
+            _area.Lexer = Lexer.Xml;
+
+            // Enable folding
+            _area.SetProperty("fold", "1");
+            _area.SetProperty("fold.compact", "1");
+            _area.SetProperty("fold.html", "1");
+
+            // Use Margin 2 for fold markers
+            _area.Margins[2].Type = MarginType.Symbol;
+            _area.Margins[2].Mask = Marker.MaskFolders;
+            _area.Margins[2].Sensitive = true;
+            _area.Margins[2].Width = 20;
+
+            // Reset folder markers
+            for (int i = Marker.FolderEnd; i <= Marker.FolderOpen; i++)
+            {
+                _area.Markers[i].SetForeColor(SystemColors.ControlLightLight);
+                _area.Markers[i].SetBackColor(SystemColors.ControlDark);
+            }
+
+            // Style the folder markers
+            _area.Markers[Marker.Folder].Symbol = MarkerSymbol.BoxPlus;
+            _area.Markers[Marker.Folder].SetBackColor(SystemColors.ControlText);
+            _area.Markers[Marker.FolderOpen].Symbol = MarkerSymbol.BoxMinus;
+            _area.Markers[Marker.FolderEnd].Symbol = MarkerSymbol.BoxPlusConnected;
+            _area.Markers[Marker.FolderEnd].SetBackColor(SystemColors.ControlText);
+            _area.Markers[Marker.FolderMidTail].Symbol = MarkerSymbol.TCorner;
+            _area.Markers[Marker.FolderOpenMid].Symbol = MarkerSymbol.BoxMinusConnected;
+            _area.Markers[Marker.FolderSub].Symbol = MarkerSymbol.VLine;
+            _area.Markers[Marker.FolderTail].Symbol = MarkerSymbol.LCorner;
+
+            // Enable automatic folding
+            _area.AutomaticFold = AutomaticFold.Show | AutomaticFold.Click | AutomaticFold.Change;
+
+            // Set the Styles
+            //_area.StyleResetDefault();
+            //// I like fixed font for XML
+            //_area.Styles[Style.Default].Font = "Courier";
+            //_area.Styles[Style.Default].Size = 10;
+            //_area.StyleClearAll();
+            _area.Styles[Style.Xml.Attribute].ForeColor = Color.Red;
+            _area.Styles[Style.Xml.Entity].ForeColor = Color.Red;
+            _area.Styles[Style.Xml.Comment].ForeColor = Color.Green;
+            _area.Styles[Style.Xml.Tag].ForeColor = Color.Blue;
+            _area.Styles[Style.Xml.TagEnd].ForeColor = Color.Blue;
+            _area.Styles[Style.Xml.DoubleString].ForeColor = Color.DeepPink;
+            _area.Styles[Style.Xml.SingleString].ForeColor = Color.DeepPink;
+        }
+
+        private static Color IntToColor(int rgb)
+        {
+            return Color.FromArgb(255, (byte)(rgb >> 16), (byte)(rgb >> 8), (byte)rgb);
+        }
+    }
+}
--- FileCompare/Presenters/FileViewPresenter.cs
+++ FileCompare/Presenters/FileViewPresenter.cs
@@ -2,6 +2,7 @@
 using System.Drawing;
 using System.IO;
 using System.Windows.Forms;
+using FileCompare.Helpers;
 using NLog;
 using ScintillaNET;
 
@@ -14,16 +15,12 @@ namespace FileCompare.Presenters
         private static Logger logger = LogManager.GetCurrentClassLogger();
         Scintilla TextArea;
 
-        public static Color HC_NODE = Color.Firebrick;
-        public static Color HC_STRING = Color.Blue;
-        public static Color HC_ATTRIBUTE = Color.Red;
-        public static Color HC_COMMENT = Color.GreenYellow;
-        public static Color HC_INNERTEXT = Color.Black;
-
         public FileViewPresenter(UCFileView fileView)
         {
             _fileView = fileView;
+            fileView.Presenter = this;
             ScintillaTextPanel = _fileView.TextPanelControl;
+            _fileView.Load += UCFileView_Load;
             _fileView.LoadFileClicked += toolStripButtonOpenFile_Click;
         }
 
@@ -37,7 +34,7 @@ namespace FileCompare.Presenters
                 if (result == DialogResult.OK)
                 {
                     TextArea.Text = File.ReadAllText(dialog.FileName);
-                    //SetFilePathAndName(dialog.FileName);
+                    ScintillaSettings.SetupXML(TextArea);
                     // TODO check file type and modify scintilla settings.
                 }
             }
@@ -52,90 +49,7 @@ namespace FileCompare.Presenters
         {
             TextArea = new Scintilla();
             ScintillaTextPanel.Controls.Add(TextArea);
-
-            //// BASIC CONFIG
-            //TextArea.Dock = DockStyle.Fill;
-
-            //// INITIAL VIEW CONFIG
-            //TextArea.WrapMode = WrapMode.None;
-            //TextArea.IndentationGuides = IndentView.LookBoth;
-
-            //// STYLING
-            //InitColors();
-            //InitSyntaxColoring();
-        }
-
-        private void InitColors()
-        {
-            TextArea.SetSelectionBackColor(true, IntToColor(0x114D9C));
-        }
-
-        private void InitSyntaxColoring()
-        {
-            // Reset the styles
-            TextArea.StyleResetDefault();
-            TextArea.Styles[Style.Default].Font = "Consolas";
-            TextArea.Styles[Style.Default].Size = 10;
-            TextArea.Styles[Style.Default].BackColor = Color.DarkGray;
-            TextArea.Styles[Style.Default].ForeColor = Color.DarkBlue;
-            TextArea.StyleClearAll();
-
-            // Set the XML Lexer
-            TextArea.Lexer = Lexer.Xml;
-
-            // Show line numbers
-            TextArea.Margins[0].Width = 20;
-
-            // Enable folding
-            TextArea.SetProperty("fold", "1");
-            TextArea.SetProperty("fold.compact", "1");
-            TextArea.SetProperty("fold.html", "1");
-
-            // Use Margin 2 for fold markers
-            TextArea.Margins[2].Type = MarginType.Symbol;
-            TextArea.Margins[2].Mask = Marker.MaskFolders;
-            TextArea.Margins[2].Sensitive = true;
-            TextArea.Margins[2].Width = 20;
-
-            // Reset folder markers
-            for (int i = Marker.FolderEnd; i <= Marker.FolderOpen; i++)
-            {
-                TextArea.Markers[i].SetForeColor(SystemColors.ControlLightLight);
-                TextArea.Markers[i].SetBackColor(SystemColors.ControlDark);
-            }
-
-            // Style the folder markers
-            TextArea.Markers[Marker.Folder].Symbol = MarkerSymbol.BoxPlus;
-            TextArea.Markers[Marker.Folder].SetBackColor(SystemColors.ControlText);
-            TextArea.Markers[Marker.FolderOpen].Symbol = MarkerSymbol.BoxMinus;
-            TextArea.Markers[Marker.FolderEnd].Symbol = MarkerSymbol.BoxPlusConnected;
-            TextArea.Markers[Marker.FolderEnd].SetBackColor(SystemColors.ControlText);
-            TextArea.Markers[Marker.FolderMidTail].Symbol = MarkerSymbol.TCorner;
-            TextArea.Markers[Marker.FolderOpenMid].Symbol = MarkerSymbol.BoxMinusConnected;
-            TextArea.Markers[Marker.FolderSub].Symbol = MarkerSymbol.VLine;
-            TextArea.Markers[Marker.FolderTail].Symbol = MarkerSymbol.LCorner;
-
-            // Enable automatic folding
-            TextArea.AutomaticFold = AutomaticFold.Show | AutomaticFold.Click | AutomaticFold.Change;
-
-            // Set the Styles
-            //TextArea.StyleResetDefault();
-            //// I like fixed font for XML
-            //TextArea.Styles[Style.Default].Font = "Courier";
-            //TextArea.Styles[Style.Default].Size = 10;
-            //TextArea.StyleClearAll();
-            TextArea.Styles[Style.Xml.Attribute].ForeColor = Color.Red;
-            TextArea.Styles[Style.Xml.Entity].ForeColor = Color.Red;
-            TextArea.Styles[Style.Xml.Comment].ForeColor = Color.Green;
-            TextArea.Styles[Style.Xml.Tag].ForeColor = Color.Blue;
-            TextArea.Styles[Style.Xml.TagEnd].ForeColor = Color.Blue;
-            TextArea.Styles[Style.Xml.DoubleString].ForeColor = Color.DeepPink;
-            TextArea.Styles[Style.Xml.SingleString].ForeColor = Color.DeepPink;
-        }
-
-        public static Color IntToColor(int rgb)
-        {
-            return Color.FromArgb(255, (byte)(rgb >> 16), (byte)(rgb >> 8), (byte)rgb);
+            ScintillaSettings.SetupScintilla(TextArea);
         }
     }
 }
